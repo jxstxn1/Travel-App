992b0223c9fb48b5bccac7c3a9d8d2f471e9b895
ATS527 <truespiritz527@gmail.com>
2021-04-26 20:36:08 +0530

    Font checking

--- travel/lib/screens/welcome_screen.dart
+++ travel/lib/screens/welcome_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final double deviceHeight = MediaQuery.of(context).size.height;
+    final double deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
         child: Stack(
@@ -13,6 +17,18 @@ class WelcomeScreen extends StatelessWidget {
               child: Image.asset('assets/images/welcome-screen-2.jpg',
                   fit: BoxFit.cover),
             ),
+            Positioned(
+              width: deviceWidth,
+              child: Text(
+                "Explore the world",
+                style: GoogleFonts.lato(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              bottom: deviceHeight * 0.32,
+            )
           ],
         ),
       ),
