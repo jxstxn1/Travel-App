c1bf4334776a1932281d9817d64db41a5a48d945
jxstxn__ <Justinbaumann12112001@gmail.com>
2021-04-26 16:46:08 +0200

    Added new package

--- travel/lib/screens/home_screen.dart
+++ travel/lib/screens/home_screen.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold();
   }
 }
