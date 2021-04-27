da8f61cc60b9872d1f4fc1cdca495f0375e0ca02
jxstxn__ <Justinbaumann12112001@gmail.com>
2021-04-27 12:23:24 +0200

    added dummy data

--- travel/lib/widgets/item_list.dart
+++ travel/lib/widgets/item_list.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
-class Item extends StatelessWidget {
-  Map<int, List<Items>> items = {
+class ItemList extends StatelessWidget {
+
+  final Map<int, List<Items>> items = {
     0: [
-      Items('Olympic National Forest', 'Washington', ''),
-      Items('Mount Kilimanjaro', 'Tanzania', ''),
+      Items('Olympic National Forest', 'Washington', 'assets/images/olympic.jpeg'),
+      Items('Mount Kilimanjaro', 'Tanzania', 'assets/images/forest.jpg'),
     ],
-    1: [],
-    2: [],
-    3: []
+    1: [
+      Items('Brandenburg Gate', 'Berlin', 'assets/images/forest.jpg'),
+      Items('Coliseum', 'Rom', 'assets/images/olympic.jpeg'),
+    ],
+    2: [
+      Items('Amsterdam', 'Netherlands', 'assets/images/forest.jpg'),
+      Items('Madeira', 'Portugal', 'assets/images/olympic.jpeg'),
+    ],
+    3: [
+      Items('Machu Picchu', 'Peru', 'assets/images/forest.jpg'),
+      Items('Amritsar', 'Punjab', 'assets/images/olympic.jpeg'),
+    ]
   };
 
   @override
