0a179bed86e0684ef5ce6f3de6cbd6437a7fe161
ATS527 <truespiritz527@gmail.com>
2021-04-30 15:13:24 +0530

    Added a splash screen

--- travel/lib/main.dart
+++ travel/lib/main.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 import 'screens/welcome_screen.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays(
+      [SystemUiOverlay.bottom, SystemUiOverlay.top]);
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]).then((_) {
     runApp(MyApp());
